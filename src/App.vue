67acadca4b0308b01832dc956b73e16e70201655
chakradhar <chakradhar@uchicago.edu>
2022-08-24 14:22:31 -0500

    first commiti

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <HelloWorld msg="Welcome to you nft app"/>
+  <hr>
+  <FileUpload />
+
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import FileUpload from './components/FileUpload.vue';
+
+export default {
+      name: 'App',
+      components: {
+        HelloWorld,
+        FileUpload,
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FileUpload.vue
@@ -0,0 +1,27 @@
+<template>
+      <input ref="file" v-on:change="handleFileUpload()"  type="file">
+</template>
+<script>
+
+import { ref} from "vue"
+
+export default{
+    name:'FileUpload',
+
+    setup() {
+        const file = ref(null)
+
+        const handleFileUpload = async() => {
+           // debugger;
+            console.log("selected file",file.value.files)
+            //do a post Request
+        }
+
+        return {
+          handleFileUpload,
+          file
+       }
+    }
+}
+
+</script>
